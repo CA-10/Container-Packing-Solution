0d2af44aebfba7d10c4cc108211cd9eab25b236b
Charbel <chaajaka@hotmail.com>
2025-10-22 22:58:09 +0100

    Added Overlap Calculations
    Renamed GA to Population,
    Added Overlap calculation code to fitness function

---
+++ Algorithms/Algorithm_GA.py
@@ -0,0 +1,3 @@
+import AlgorithmBase
+
+#TODO
--- GA/Member.py
+++ GA/Member.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 class Member:
     
@@ -7,3 +8,22 @@ class Member:
         
         #Randomly generate the Genome upon construction
         self.genome = [(random.randint(0, container_width), random.randint(0, container_height)) for _ in range(num_genes)]
+    
+    def calculate_overlap(self, radii):
+        penalty = 0
+    
+        #For each circle, loop over each circle again apart from current and calculate overlap between all circles.
+        for i in range(len(self.genome) - 1):
+            for j in range(i + 1, len(self.genome) - 1):
+                #Use the Euclidian distance formuala: d = sqrt((xi - xj)^2 + (yi - yj)^2)
+                dx = self.genome[i][0] - self.genome[j][0]
+                dy = self.genome[i][1] - self.genome[j][1]
+                dist = math.sqrt(dx**2 + dy**2)
+                
+                #d >= ri + rj
+                overlap = radii[i] + radii[j] - dist
+                
+                if overlap > 0:
+                    penalty += overlap ** 2 #Add a squared penalty as we want to HEAVILY discourage overlapping.
+        
+        return overlap
